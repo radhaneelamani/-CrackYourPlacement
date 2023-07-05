c1f6c1d1b560399d9208588d2016f743dea8abd9
radhaneelamani <105295708+radhaneelamani@users.noreply.github.com>
2023-07-05 20:47:53 +0530

    Create Reverse Integer.py

---
+++ Reverse Integer.py
@@ -0,0 +1,11 @@
+# Problem: https://leetcode.com/problems/reverse-integer/
+class Solution:
+    def reverse(self, x: int) -> int:
+        if x >= 0:
+            y = int(str(x)[::-1])
+            return y if y < 2147483648 else 0
+        else:
+            y = -int(str(x)[:0:-1])
+            return y if y > -2147483648 else 0
+
+            
